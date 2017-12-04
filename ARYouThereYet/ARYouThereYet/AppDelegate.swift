661c6705acc8e03db869eb5218774a4ffe8e4c33
Amlaan Bhoi <amlaanb@gmail.com>
2017-12-04 14:42:51 -0600

    Added launchscreen

--- ARYouThereYet/ARYouThereYet/AppDelegate.swift
+++ ARYouThereYet/ARYouThereYet/AppDelegate.swift
@@ -20,7 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSPlacesClient.provideAPIKey("AIzaSyCKDMKwzaeKVKXhxFZO-p1otaEuQ2h3Vf4")
-
         return true
     }
 
